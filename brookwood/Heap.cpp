56b7a623a3a7f61baaaa9a993330daf9fe02d20c
bgonzax <bgonzales20001@gmail.com>
2015-05-13 12:59:00 -0700

    add heap

---
+++ brookwood/Heap.cpp
@@ -0,0 +1,11 @@
+#include "Heap.h"
+
+
+Heap::Heap()
+{
+}
+
+
+Heap::~Heap()
+{
+}
---
+++ brookwood/Heap.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "stdafx.h"
+
+class Heap
+{
+public:
+	Heap();
+	~Heap();
+
+private:
+	BYTE *heap;
+};
+
