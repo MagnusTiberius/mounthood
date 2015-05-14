bd8732ec307b5996be8498d58f39c16dafb13ab3
bgonzax <bgonzales20001@gmail.com>
2015-05-14 16:27:33 -0700

    allocator testing

--- brookwood/AllocatorB.cpp
+++ brookwood/AllocatorB.cpp
@@ -14,4 +14,77 @@ AllocatorB::AllocatorB(size_t item_size)
 
 AllocatorB::~AllocatorB()
 {
+	LPNODE node = lpBackHeap;
+	while (node->prev != NULL)
+	{
+		free(node->bitmap);
+		node->bitmap = NULL;
+		free(node->heap_block);
+		node->heap_block = NULL;
+		node = node->prev;
+		memset(node->next, 0, sizeof(NODE));
+		free(node->next);
+	}
+}
+
+
+void AllocatorB::Add(LPNODE lpNode)
+{
+	if (lpRootHeap == NULL)
+	{
+		lpRootHeap = lpNode;
+		lpBackHeap = lpNode;
+		return;
+	}
+	lpBackHeap->next = lpNode;
+	lpNode->prev = lpBackHeap;
+	lpBackHeap = lpNode;
+}
+
+AllocatorB::LPNODE AllocatorB::New()
+{
+	LPNODE lpNode = (LPNODE)malloc(sizeof(NODE));
+	memset(lpNode, 0, sizeof(NODE));
+	lpNode->heap_block = (char*)malloc(BLOCKSIZE);
+	memset(lpNode->heap_block, 0, BLOCKSIZE);
+	lpNode->bitmap_count = (BLOCKSIZE / m_grain_size);
+	lpNode->bitmap = (char*)malloc(lpNode->bitmap_count);
+	memset(lpNode->bitmap, 0, lpNode->bitmap_count);
+	lpNode->next = NULL;
+	lpNode->prev = NULL;
+	lpNode->allocated = 0;
+	lpNode->node_id = ctr++;
+	return lpNode;
+
+}
+
+void* AllocatorB::Reserve(size_t size)
+{
+	return AllocFromNode(lpRootHeap, size);
+}
+
+void* AllocatorB::AllocFromNode(LPNODE lpNode, size_t size)
+{
+	void* item;
+	int n = lpNode->bitmap_count;
+	for (int i = 0; i < n; i++)
+	{
+		if (lpNode->bitmap[i] == 0)
+		{
+			lpNode->bitmap[i] = 1;
+			item = lpNode->heap_block + (i * size);
+			lpNode->allocated += size;
+			return item;
+		}
+	}
+	if (lpNode->next != NULL)
+	{
+		return AllocFromNode(lpNode->next, size);
+	}
+	if (lpNode->next == NULL)
+	{
+		LPNODE new_node = New();
+		Add(new_node);
+		return AllocFromNode(new_node, size);
+	}
 }
--- brookwood/AllocatorB.h
+++ brookwood/AllocatorB.h
@@ -14,6 +14,7 @@ public:
 	~AllocatorB();
 
 	typedef struct node {
+		int node_id;
 		char* heap_block;
 		struct node *prev;
 		struct node *next;
@@ -23,72 +24,18 @@ public:
 	}NODE, *LPNODE;
 
 
-	void* reserve(size_t size)
-	{
-		return alloc_from_node(lpRootHeap, size);
-	}
+	void* Reserve(size_t size);
 
 private:
-	void* alloc_from_node(LPNODE lpNode, size_t size)
-	{
-		void* item;
-		int n = lpNode->bitmap_count;
-		for (int i = 0; i < n; i++)
-		{
-			if (lpNode->bitmap[i] == 0)
-			{
-				lpNode->bitmap[i] = 1;
-				item = lpNode->heap_block + (i * size);
-				lpNode->allocated += size;
-				return item;
-			}
-		}
-		if (lpNode->next != NULL)
-		{
-			return alloc_from_node(lpNode->next, size);
-		}
-		if (lpNode->next == NULL)
-		{
-			LPNODE new_node = New();
-			Add(new_node);
-			return alloc_from_node(new_node, size);
-		}
-	}
-
-private:
-	void Add(LPNODE lpNode)
-	{
-		if (lpRootHeap == NULL)
-		{
-			lpRootHeap = lpNode;
-			lpBackHeap = lpNode;
-			return;
-		}
-		lpBackHeap->next = lpNode;
-		lpNode->prev = lpBackHeap;
-		lpBackHeap = lpNode;
-	}
-
-	LPNODE New()
-	{
-		LPNODE lpNode = (LPNODE)malloc(sizeof(NODE));
-		memset(lpNode, 0, sizeof(NODE));
-		lpNode->heap_block = (char*)malloc(BLOCKSIZE);
-		memset(lpNode->heap_block, 0, BLOCKSIZE);
-		lpNode->bitmap_count = (BLOCKSIZE / m_grain_size);
-		lpNode->bitmap = (char*)malloc(lpNode->bitmap_count);
-		memset(lpNode->bitmap, 0, lpNode->bitmap_count);
-		lpNode->next = NULL;
-		lpNode->prev = NULL;
-		lpNode->allocated = 0;
-		return lpNode;
-
-	}
+	void* AllocFromNode(LPNODE lpNode, size_t size);
+	void Add(LPNODE lpNode);
+	LPNODE New();
 
 private:
 	LPNODE lpRootHeap;
 	LPNODE lpBackHeap;
 	size_t m_grain_size;
 	size_t index;
+	int ctr = 0;
 };
 
